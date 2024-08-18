54a00aed23a5617b46b31e5b7d056a297f1138c6
MagnumWave <aureliomagno@terra.com.br>
2024-08-18 17:34:02 -0300

    finalizando o exercício

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu número de conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu primeiro nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Digite seu saldo: ");
+        BigDecimal saldo = BigDecimal.valueOf(scanner.nextDouble());
+        
+        //TODO: devemos aplicar validações e restrições aos valores dos inputs.
+        //agencia que não aceita letras e outros caracteres especiais
+        //nome com trim
+        //saldo que não pode começar negativo, etc...
+
+        System.out.println(
+            String.format("Olá %s %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque",
+                nomeCliente, sobrenomeCliente, agencia, conta, saldo.doubleValue())
+        );
+
     }
 }
